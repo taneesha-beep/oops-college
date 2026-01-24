635877b72d897d7854258e8a39ba544e208555c5
Taneesha Badhe <taneeshabadhe@gmail.com>
2026-01-24 09:24:50 +0530

    Add files via upload

---
+++ ass7.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{
+    // Traverse the array
+    for (int i = 0; i < n - 1; i++)
+    {
+        // Last i elements are already in place
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            // Compare and swap if the element is greater than the next
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+// Function to print the array
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: \n";
+    printArray(arr, n);
+    bubbleSort(arr, n);
+
+    cout << "Sorted array: \n";
+    printArray(arr, n);
+
+    return 0;
+}
